83077bfd5e02f16f9f17bfbe8e8eab8d42d6e043
rojojun <zoloman316@gmail.com>
2023-11-15 19:54:10 +0900

    [fix] TEST코드 실제 적용 수정

--- src/test/java/com/rojojun/coyukdaebe/service/CommonServiceTest.java
+++ src/test/java/com/rojojun/coyukdaebe/service/CommonServiceTest.java
@@ -1,11 +1,15 @@
-/*
 package com.rojojun.coyukdaebe.service;
 
 import com.rojojun.coyukdaebe.dto.NicknameRequestDto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles({"dev"})
+@SpringBootTest
 class CommonServiceTest {
 
     @Autowired
@@ -16,6 +20,6 @@ class CommonServiceTest {
         NicknameRequestDto nicknameRequestDto = new NicknameRequestDto("lala");
         String nickname = commonService.createNickname(nicknameRequestDto);
 
-        Assertions.assertEquals("test", nickname);
+        Assertions.assertEquals("lala", nickname);
     }
-}*/
+}
